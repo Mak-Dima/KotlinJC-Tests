bd09017e86ecd80eb650cc3ca6c5c879b00efe53
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-01-19 18:28:11 -0600

    Ktor dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
